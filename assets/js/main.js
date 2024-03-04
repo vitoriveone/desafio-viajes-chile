886a4b360ea8d486ad9570d8fba745b4f7ef3f66
Víctor Riveros Rivas <vitorive.one@gmail.com>
2024-03-04 20:37:46 -0300

    Agregando Carousel...

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,9 @@
 $(document).ready(function () {
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+    const myModal = new bootstrap.Modal(document.getElementById('modal'));
 
+    $('.carousel-button').on('click', function (event) {
+        myModal.show();
+    })
 });
